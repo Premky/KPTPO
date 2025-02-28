67553c5d799fa9a369c08c667929c5080442702f
Prem Kumar Yadav <premnsa108@gmail.com>
2025-02-28 14:47:51 +0545

    2-28-25-1.12

--- Frontend/src/Components/Auth/middlewares/LoggedIn.jsx
+++ Frontend/src/Components/Auth/middlewares/LoggedIn.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import {useAuth} from '../../../Context/AuthContext';
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../../Context/AuthContext";
+
 const LoggedIn = () => {
-    const { state } = useAuth();
-    // console.log(state);
-    const isValidUser = state.valid; // Ensure boolean check    
-    return isValidUser ? <Navigate to="/" replace /> : <Outlet />; // Only redirect if logged in
+  const { state } = useAuth();
+  const isValidUser = !!state?.valid; // Ensure boolean check
+
+  return isValidUser ? <Navigate to="/" replace /> : <Outlet />;
 };
 
 export default LoggedIn;
