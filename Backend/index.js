11a12361fda1da5f53212796bce818ec6c9b1931
Premky <42573609+Premky@users.noreply.github.com>
2025-04-22 22:55:49 +0545

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -55,7 +55,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: false, // Set to true if you're using HTTPS in production
+        secure: true, // Set to true if you're using HTTPS in production
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     }
 }));
